71723e1fa9f10b2d8c4bef1c35fe5243e359bf0d
Daniel Trad <danielotrad@gmail.com>
2022-05-16 16:23:48 -0600

    testing calls

--- hpctests/test_Inv.cpp
+++ hpctests/test_Inv.cpp
@@ -14,51 +14,61 @@
 #include <complex>
 #include <time.h>
 
-void test() {
-    return;
+using namespace std;
+
+std::vector<Eigen::MatrixXcf> mytestfunc(int nrows, int ncols, int nvector) {
+// testing returning by value and allocating/deallocating each time.
+    
+    std::vector<Eigen::MatrixXcf> MADI(nvector);
+    for (auto i = 0; i < nvector; i++)
+        MADI[i] = Eigen::MatrixXcf::Zero(nrows, ncols);
+
+    return MADI;
 }
 
-std::vector<Eigen::MatrixXcf> mytestfunc();
+void mytestfunc2(std::vector<Eigen::MatrixXcf>& MADI) {
+    
+    //testing passing externally allocated.
+    auto n=MADI.size();
+    //cerr << "vector size " << n << endl;
+    for (auto i = 0; i < n; i++) 
+        MADI[i].setZero();
+}
+
+std::vector<Eigen::MatrixXcf> mytestfunc(int nrows, int ncols);
 void mytestfunc2(std::vector<Eigen::MatrixXcf>& MADI);
 int main() {
-    timer* timer0 = new timer();
 
+    int ncalls=1000;
+    int nrows=100;
+    int ncols=100;
+    int nvector=100;
+    
     {
+        timer* timer0 = new timer();
         timer0->start();
-        for (auto i = 0; i < 10; i++)
-            std::vector<Eigen::MatrixXcf> D = mytestfunc();
+        for (auto i = 0; i < ncalls; i++)
+            std::vector<Eigen::MatrixXcf> D = mytestfunc(nrows, ncols, nvector);
         timer0->end();
-        std::cerr << "timer0" << timer0->totalTime;
+        std::cerr << "timer0->" << timer0->totalTime << endl;
     }
 
     {
         
-        int n = 1000;
-        std::vector<Eigen::MatrixXcf> MADI(n);
-        for (auto i = 0; i < n; i++)
-            MADI[i] = Eigen::MatrixXcf(1000, 1000);
+        timer* timer1 = new timer();
+        timer1->start();
+        std::vector<Eigen::MatrixXcf> MADI(nvector);
+        for (auto i = 0; i < nvector; i++)
+            MADI[i] = Eigen::MatrixXcf(nrows, ncols);
         
-        timer0->start();
-        for (auto i = 0; i <10;i++)
+        
+        for (auto i = 0; i <ncalls;i++)
             mytestfunc2(MADI);
-        timer0->end();
-        std::cerr << "timer1" << timer0->totalTime;
+        timer1->end();
+        std::cerr << "timer1->" << timer1->totalTime;
 
     }
     return 0;
 
 }
 
-std::vector<Eigen::MatrixXcf> mytestfunc() {
-    int n = 1000;
-    std::vector<Eigen::MatrixXcf> MADI(n);
-    for (auto i = 0; i < n; i++)
-        MADI[i] = Eigen::MatrixXcf(1000, 1000);
-
-    return MADI;
-}
-
-void mytestfunc2(std::vector<Eigen::MatrixXcf>& MADI) {
-    for (auto i = 0; i < MADI.size(); i++) 
-        MADI[i].setZero();
-}
